97a9d11fdd1a3970e7c0dab93fb809964b21930c
Pozzitron1337 <40902039+Pozzitron1337@users.noreply.github.com>
2019-02-02 00:39:59 +0200

    A little bit was corrected Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Main {
 
     public static void main(String[] args) {
-        DriversProperties.FirefoxProperty();
 
-        /*WebDriver driver =new FirefoxDriver();
-        driver.get("http:\\google.com");
-        System.out.println(driver.getCurrentUrl());
-        WebElement search=driver.findElement(By.name("q"));
-        System.out.println(search);*/
+
         ParseMemes.parseMemeFromvkcom();
 
     }
